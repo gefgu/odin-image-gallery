3c78fae9bb88b0afafa282fe1f17b30e02b5f10b
gefgu <gefgu@hotmail.com>
2022-04-03 19:37:45 -0300

    Add show first image of gallery dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "./style.css"
+import "./style.css";
+
+(() => {
+  const imageInDisplayContainer = document.querySelector(
+    ".slider-current-image"
+  );
+  const imagesElementList = document.querySelectorAll(".slider-images > img");
+
+  imageInDisplayContainer.appendChild(imagesElementList[0]);
+})();
