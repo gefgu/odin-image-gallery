eedd51bed0ab34f847392a2075cf20e6f6671f36
gefgu <gefgu@hotmail.com>
2022-04-03 19:53:32 -0300

    Add advance slide functionality with cleaning and activation functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@ import "./style.css";
 
 (() => {
   const imagesElementList = document.querySelectorAll(".slider-images > img");
-  imagesElementList[0].classList.add("active");
+  let imageIndex = 0;
+ 
+  const makeImageActive = () => {
+    imagesElementList[imageIndex].classList.add("active");
+  }
+ 
+  const cleanSlider = () => {
+    const currentImage = document.querySelector(".slider-images > img.active");
+    currentImage.classList.remove("active");
+  }
+
+  const advanceSlider = () => {
+    cleanSlider();
+    if (imageIndex + 1 < imagesElementList.length) {
+      imageIndex += 1;
+    } else {
+      imageIndex = 0;
+    }
+    makeImageActive();
+  }
+
+  makeImageActive();
+  advanceSlider();
+
 })();
