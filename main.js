72696b490e326a79c409cee77e4a8d156594107c
gefgu <gefgu@hotmail.com>
2022-04-03 19:37:45 -0300

    Add show first image of gallery dynamically

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\n//# sourceURL=webpack://image-slider-module/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\n(() => {\n  const imageInDisplayContainer = document.querySelector(\n    \".slider-current-image\"\n  );\n  const imagesElementList = document.querySelectorAll(\".slider-images > img\");\n\n  imageInDisplayContainer.appendChild(imagesElementList[0]);\n})();\n\n\n//# sourceURL=webpack://image-slider-module/./src/index.js?");
 
 /***/ })
 
