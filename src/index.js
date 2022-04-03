7fae8ce23d16e7a48d64f34e58feda0d9402ddd1
gefgu <gefgu@hotmail.com>
2022-04-03 19:44:00 -0300

    Add class method for showing first image of gallery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "./style.css";
 
 (() => {
-  const imageInDisplayContainer = document.querySelector(
-    ".slider-current-image"
-  );
   const imagesElementList = document.querySelectorAll(".slider-images > img");
-
-  imageInDisplayContainer.appendChild(imagesElementList[0]);
+  imagesElementList[0].classList.add("active");
 })();
