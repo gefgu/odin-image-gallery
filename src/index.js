f82d83955847a2cb1be1c8c5e280c4e722ca5b55
gefgu <gefgu@hotmail.com>
2022-04-07 15:12:46 -0300

    Add moving slider using scrolling

--- src/index.js
+++ src/index.js
@@ -2,36 +2,15 @@ import "./style.css";
 
 (() => {
   const imageGallery = document.querySelector(".image-slider-container");
-  const imagesElementList = document.querySelectorAll(".slider-images > img");
-  let imageIndex = 0;
-
-  const makeImageActive = () => {
-    imagesElementList[imageIndex].classList.add("active");
-  };
-
-  const cleanSlider = () => {
-    const currentImage = document.querySelector(".slider-images > img.active");
-    currentImage.classList.remove("active");
-  };
+  const imageSlider = document.querySelector(".slider-images");
+  const imageSize = 900;
 
   const advanceSlider = () => {
-    cleanSlider();
-    if (imageIndex + 1 < imagesElementList.length) {
-      imageIndex += 1;
-    } else {
-      imageIndex = 0;
-    }
-    makeImageActive();
+    imageSlider.scroll({ left: imageSlider.scrollLeft + imageSize, behavior: "smooth" });
   };
 
   const recedeSlider = () => {
-    cleanSlider();
-    if (imageIndex - 1 < 0) {
-      imageIndex = imagesElementList.length - 1;
-    } else {
-      imageIndex -= 1;
-    }
-    makeImageActive();
+    imageSlider.scroll({ left: imageSlider.scrollLeft - imageSize, behavior: "smooth" });
   };
 
   const createSliderArrows = () => {
@@ -57,6 +36,4 @@ import "./style.css";
   };
 
   createSliderArrows();
-
-  makeImageActive();
 })();
