a0d19891192a00d95152c31b27623560f291ef70
gefgu <gefgu@hotmail.com>
2022-04-07 15:44:52 -0300

    Add dynamically added images with styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ import "./style.css";
   const imageSlider = document.querySelector(".slider-images");
   const imageSize = 900;
 
+  // Create Images
+  (() => {
+    const getImagesSources = () => {
+      let listOfSources = [];
+      for (let i = 0; i < 5; i++) {
+        const randomID = Math.floor(Math.random() * 201);
+        listOfSources.push(`https://picsum.photos/id/${randomID}/900/600`)
+      }
+      return listOfSources;
+    }
+
+    getImagesSources().forEach(source => {
+      const container = document.createElement("div");
+      container.style.backgroundImage = `url("${source}")`;
+      imageSlider.appendChild(container);
+    })
+  })();
+
   const advanceSlider = () => {
     imageSlider.scroll({ left: imageSlider.scrollLeft + imageSize, behavior: "smooth" });
   };
